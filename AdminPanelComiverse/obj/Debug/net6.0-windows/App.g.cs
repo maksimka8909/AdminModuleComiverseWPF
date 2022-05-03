5e45b4bd0322f614c8d06701c962e96c8cdd6c54
maksimka8909 <kuznec-2015@mail.ru>
2022-05-04 00:09:24 +0300

    04.05.2022

--- AdminPanelComiverse/obj/Debug/net6.0-windows/App.g.cs
+++ AdminPanelComiverse/obj/Debug/net6.0-windows/App.g.cs
@@ -44,7 +44,7 @@ namespace AdminPanelComiverse {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.2.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.1.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -59,7 +59,7 @@ namespace AdminPanelComiverse {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.2.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.1.0")]
         public static void Main() {
             AdminPanelComiverse.App app = new AdminPanelComiverse.App();
             app.InitializeComponent();
